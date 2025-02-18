ceacfe0c0b7302a57720bd9dfa43ad89571a5331
Karan Vaishnav <krn.vaishnav11@gmail.com>
2025-02-18 08:29:16 +0530

    done with installations of express and mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,33 @@
-const x: number = 1;
-console.log(x);
+import express from "express"; 
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+
+})
+
+app.post("/api/v1/signin", (req, res) => {
+    
+})
+
+
+app.post("/api/v1/content", (req, res) => {
+    
+})
+
+app.get("/api/v1/content", (req, res) => {
+    
+})
+
+app.delete("/api/v1/content", (req, res) => {
+    
+})
+
+app.post("/api/v1/secondbrain/share", (req, res) => {
+    
+})
+
+app.get("/api/v1/secondbrain/:shareLink", (req, res) => {
+    
+})
