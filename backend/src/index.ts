6c66e384dc2bcf8845f08616d3e781c15ff24100
Karan Vaishnav <85957167+karan-vaishnav@users.noreply.github.com>
2025-02-25 12:20:48 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://secondbrain-ly.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
