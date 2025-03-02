2f8a21a46c92a31415c55e36e4d459eedd9f5a35
Karan Vaishnav <krn.vaishnav11@gmail.com>
2025-03-02 23:44:38 +0530

    cors error2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,11 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 app.post("/api/v1/signup", async (req, res) => {
   const UserSchema = z.object({
     username: z
