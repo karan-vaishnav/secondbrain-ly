35f4532a1012c805b2c9b5a48eccd7a5b1627b77
Karan Vaishnav <krn.vaishnav11@gmail.com>
2025-03-03 00:14:00 +0530

    cors errors

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -22,8 +22,17 @@ const middleware_1 = require("./middleware");
 const utils_1 = require("./utils");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: "https://secondbrain-ly.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", (0, cors_1.default)());
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
 app.post("/api/v1/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const UserSchema = zod_1.z.object({
         username: zod_1.z
