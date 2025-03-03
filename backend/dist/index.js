4d71ca099bd65087f81e57cb329b398a7e58af5b
Karan Vaishnav <krn.vaishnav11@gmail.com>
2025-03-03 10:19:23 +0530

    made changes in vercel.json

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -22,12 +22,13 @@ const middleware_1 = require("./middleware");
 const utils_1 = require("./utils");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: "https://secondbrain-ly.vercel.app",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-}));
-app.options("*", (0, cors_1.default)());
+app.use((0, cors_1.default)());
+// app.use(cors({
+//   origin: "https://secondbrain-ly.vercel.app",
+//   methods: "GET,POST,PUT,DELETE",
+//   credentials: true
+// }));
+// app.options("*", cors());
 app.use(express_1.default.json());
 app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.url}`);
